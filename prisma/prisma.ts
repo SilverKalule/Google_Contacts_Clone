54bbc63875f7e19848fb4a5e0ae5889215371808
SilverKalule <162054797+SilverKalule@users.noreply.github.com>
2025-06-18 18:47:00 +0300

    changed prisma generate

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../lib/generated/prisma";
+import { PrismaClient } from "./generated/prisma";
 /**new changes */
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
